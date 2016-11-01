d0eee75c29b8a56f4c8db376d86cb560c93f4b1f
ZerrickStaples <zerrick.staples@gmail.com>
2016-11-01 17:39:01 -0400

    Added website files.

---
+++ EightBall/script.js
@@ -0,0 +1,10 @@
+﻿function GetFortune() {
+    var Options =
+        [
+            "Yes", "No", "Maybe", "So", "I don't know", "I think that is very possible", "Why would you ask me that?", "You should try it and see.",
+            "Haha", "I need more information.", "Ummm... Maybe try again tomorrow."
+        ];
+    var rand = Options[Math.floor(Math.random() * Options.length)];
+    document.getElementById("Fortune").innerHTML = rand;
+
+}
